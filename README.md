65a4790d205d4ec3c1951c61b79ac13e4341aeff
Deirdre18 <deirdreweldon18@gmail.com>
2021-02-18 12:47:07 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Superhero Science Fantasy App
+
+## Credits
+
+# Sources of Code
+https://ionicacademy.com/ionic-crash-course/-(used code in tutorials for basic setup of app, routing, etc)
+https://github.com/herbae/starwarsapp (took code from here for people and person-details pages)
+
+# Sources of Images
+
+
+# Icon and Splash Images
+
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBNyt0cNfmIn_1fEQ_mFqPGmKQHllyEhsA",
+    authDomain: "superhero-science-fantasy.firebaseapp.com",
+    projectId: "superhero-science-fantasy",
+    storageBucket: "superhero-science-fantasy.appspot.com",
+    messagingSenderId: "540082137140",
+    appId: "1:540082137140:web:1933b483d239cb8f354261",
+    measurementId: "G-0R6VXBLZ99",
+    databaseURL: ""
+  }
+};
